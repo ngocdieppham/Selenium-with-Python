33e0e9175c48996d4519c1e33d8b4b5f4154a1d2
Điệp Phạm <75340446+ngocdieppham@users.noreply.github.com>
2024-03-27 05:52:35 +0700

    Delete testsuite.py

--- testsuite.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-from searchTests import SearchTests
-from homepageTest import HomePageTest
-
-search_test = unittest.TestLoader().loadTestsFromTestCase(SearchTests)
-home_page_test = unittest.TestLoader().loadTestsFromTestCase(HomePageTest)
-smoke_test = unittest.TestSuite([search_test,home_page_test])
-unittest.TextTestRunner(verbosity=2).run(smoke_test)
