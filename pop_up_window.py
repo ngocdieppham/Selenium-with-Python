46cda2dc3fbf030a255ba6ea0541fbb41780047c
Ngoc Diep <ngocdiep0919@gmail.com>
2024-03-27 06:16:18 +0700

    change comment

--- pop_up_window.py
+++ pop_up_window.py
@@ -6,13 +6,13 @@ class PopUpWindowTest(unittest.TestCase):
         self.driver = webdriver.Chrome()
         self.driver.implicitly_wait(3)
         self.driver.maximize_window()
-        self.driver.get('http://www.lazada.vn')
+        self.driver.get('') # enter URL
 
     def test_pop_up_window(self):
         driver=self.driver
-        # lưu windowhandle cửa số trình duyệt cha - Parent Browser Window vào 1 biến
+        # save windowhandle - Parent Browser Window into a variable
         parrent_window = driver.current_window_handle
-        # open của sổ con pop_up window hay child window
+        # open pop_up window or child window
         btn_help = driver.find_element_by_id('helpbutton')
         btn_help.click()
         driver.switch_to_window('HelpWindow')
@@ -24,5 +24,5 @@ class PopUpWindowTest(unittest.TestCase):
         self.driver.quit()
 
 if __name__=='__main__':
-    unittest.main(verbosity=2)
+    unittest.main(verbosity=1)
 
