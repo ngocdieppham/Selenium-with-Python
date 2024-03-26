b2194fa75a257dc60c6e0b5360f43ea3b8a4c481
Ngoc Diep <ngocdiep0919@gmail.com>
2024-03-26 10:54:58 +0700

    enter URL, check and update script

--- wait_enabled.py
+++ wait_enabled.py
@@ -6,25 +6,27 @@ from selenium.webdriver.support import expected_conditions
 
 class WaitEnabled(unittest.TestCase):
     @classmethod
-    def setUpClass(cls):
-        cls.driver = webdriver.Chrome()
-        cls.driver.implicitly_wait(3)
-        cls.driver.maximize_window()
-        cls.driver.get('')
-        WebDriverWait(cls.driver,3).until(expected_conditions.title_contains('Shopping online - Buy online on Lazada.vn'))
+    def setUpClass(self):
+        self.driver = webdriver.Chrome()
+        self.driver.implicitly_wait(3)
+        self.driver.maximize_window()
+        self.driver.get('https://www.lazada.vn/')
+        WebDriverWait(self.driver,3).until(expected_conditions.title_contains('Lazada - Mua Sắm Hàng Chất Giá Tốt Online'))
 
     def test_account(self):
         driver = self.driver
-        login_button = WebDriverWait(driver,5).until(expected_conditions.element_to_be_clickable((By.LINK_TEXT,'LOGIN')))
+        #check LOGIN is clickable
+        login_button = WebDriverWait(driver,10).until(expected_conditions.element_to_be_clickable((By.LINK_TEXT,'LOGIN')))
         login_button.click()
-        WebDriverWait(driver,3).until(expected_conditions.title_contains('Lazada.vn: Online Shopping Vietnam'))
-        create_button = WebDriverWait(driver,5).until(expected_conditions.element_to_be_clickable((By.LINK_TEXT,'SIGNUP')))
+        self.assertEqual('Welcome to Lazada! Please login.',driver.find_element_by_xpath('/html/body/div[2]/div/div[2]/div/div[1]/h3').text)
+        #check SIGNUP is clickable
+        create_button = WebDriverWait(driver,10).until(expected_conditions.element_to_be_clickable((By.LINK_TEXT,'SIGNUP')))
         create_button.click()
-        self.assertEqual('Create your Lazada Account',driver.find_element_by_xpath("//h3[contains(text(),'Create your Lazada Account')]").text)
+        self.assertEqual('Create your Lazada Account',driver.find_element_by_xpath("//*[@id='container']/div/div[1]/h3").text)
 
     @classmethod
-    def tearDownClass(cls):
-        cls.driver.quit()
+    def tearDownClass(self):
+        self.driver.quit()
 
 if __name__=='__main__':
-    unittest.main(verbosity=2)
+    unittest.main(verbosity=1)
